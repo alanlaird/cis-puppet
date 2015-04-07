739cf754512c03fd26972264f5f97d0e8673508a
Alan Laird <alan.laird@iovation.com>
2015-04-07 13:10:12 -0700

    removed redundant code

--- manifests/linuxcontrols/c0014.pp
+++ manifests/linuxcontrols/c0014.pp
@@ -10,23 +10,6 @@ class cis::linuxcontrols::c0014 {
   #  group   => root,
   #  mode    => '0640',
   #}
-  augeas { "limits_conf_core":
-    $domain   = '*',
-    $type     = 'hard',
-    $item     = 'core',
-    $value    = 0,
-    $context  = '/files/etc/security/limits.conf',
-    $path_list  = "domain[.=\"$domain\"][./type=\"$type\" and ./item=\"$item\"]"
-    $path_exact = "domain[.=\"$domain\"][./type=\"$type\" and ./item=\"$item\" and ./value=\"$value\"]"
-    changes => [
-      # remove all matching to the $domain, $type, $item, for any $value
-      "rm $path_list", 
-      # insert new node at the end of tree
-      "set domain[last()+1] $domain",
-      # assign values to the new node
-      "set domain[last()]/type $type",
-      "set domain[last()]/item $item",
-      "set domain[last()]/value $value",
-    ],
-  }
+  # 
+  # covered by c0015.pp.  nothing to see here
 }
