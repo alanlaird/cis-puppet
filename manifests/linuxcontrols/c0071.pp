082c99c6bd70f7fda78426365e74541a4c1f35f6
Travis Ogdon <travis.ogdon@iovation.com>
2015-11-04 14:50:33 -0800

    Fixing the passwd issue reported in INF-408

--- manifests/linuxcontrols/c0071.pp
+++ manifests/linuxcontrols/c0071.pp
@@ -11,7 +11,13 @@ class cis::linuxcontrols::c0071 {
     target  => '/etc/pam.d/system-auth-ac',
   }
   file {'/etc/pam.d/system-auth-ac':
-    source  => 'puppet:///modules/cis/el6/etc/pam.d/system-auth-ac',
+    source  => 'puppet:///modules/cis/el7/etc/pam.d/system-auth-ac',
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+  }
+  file {'/etc/security/pwquality.conf':
+    source  => 'puppet:///modules/cis/el7/etc/security/pwquality.conf',
     owner   => root,
     group   => root,
     mode    => '0644',
